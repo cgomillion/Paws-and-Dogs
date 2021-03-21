bd4a08549fbac435ba30948bc3499538d1fc4f01
Corey Gomillion <coreygomillion@Coreys-MacBook-Pro.local>
2021-03-21 15:00:20 -0400

    Home page complete, just need to add new page link, and index page is 90 percent complete minor styling details, next is to complete the new route and page

--- controllers/pawsanddogs.js
+++ controllers/pawsanddogs.js
@@ -27,33 +27,44 @@ router.get('/seed', (req, res) => {
         {
             name: 'Huncho',
             breed: 'American Bully',
-            description: '',
-            img: '',
+            description: "I am the coolest, cutest, funniest smedium dog you will ever meet! I love to follow my dad around and take naps at any given time; maybe that's why they call me 'Break Time'",
+            img: '/assets/F48BFAD3-AA0E-4544-9220-7CE0E5C42B1E_1_105_c.jpeg',
             age: 2,
             loveable: 9
         },
         {
             name: 'Stella',
             breed: 'Staffordshire Bully',
-            description: '',
-            img: '',
+            description: 'I am the juiciest, prettiest, and sweetest dog of them all. I love my bellyrubs and I will let you know when they end, not you.',
+            img: '/assets/1CB2DB6C-240D-4708-8F8F-4650EC214F15_1_105_c.jpeg',
             age: 5,
             loveable: 10
         },
-        {
-            name: 'Bear',
-            breed: '',
-            description: '',
-            img: '',
-            age: 6,
-            loveable: 8
-        }
+        // {
+        //     name: 'Bear',
+        //     breed: '',
+        //     description: '',
+        //     img: '',
+        //     age: 6,
+        //     loveable: 8
+        // }
     ], (err, data) => {
         if (err) {
             console.log(err)
         }
-        res.redirect('/dogs')
+        res.redirect('/pawsanddogs')
     })
 });
+
+// SHOW ROUTE
+router.get('/:id', (req, res)=>{
+    console.log(req.params.id)
+    
+    Dog.findById(req.params.id, (err, foundDog)=>{
+        res.render('show.ejs', {
+            dog: foundDog
+        })
+    })
+  });
 
 module.exports = router;
